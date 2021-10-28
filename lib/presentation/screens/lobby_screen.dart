8321287c5d59fa98a0de724e1a99f3cd17b0611e
Tio Irawan <sattanationmail@gmail.com>
2021-10-28 23:04:58 +0700

    Lobby design

--- lib/presentation/screens/lobby_screen.dart
+++ lib/presentation/screens/lobby_screen.dart
@@ -1,8 +1,10 @@
+import 'package:entry/entry.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:get/get.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:quizpancasila/domain/entities/room.dart';
+import 'package:quizpancasila/presentation/constants/colors.dart';
 import 'package:quizpancasila/presentation/controllers/auth_controller.dart';
 import 'package:quizpancasila/presentation/controllers/lobby_controller.dart';
 import 'package:quizpancasila/presentation/screens/countdown_screen.dart';
@@ -10,6 +12,7 @@ import 'package:quizpancasila/presentation/screens/home_screen.dart';
 import 'package:quizpancasila/presentation/screens/leaderbord_screen.dart';
 import 'package:quizpancasila/presentation/screens/quiz_screen.dart';
 import 'package:quizpancasila/presentation/widgets/participant_item.dart';
+import 'package:responsive_grid/responsive_grid.dart';
 
 class LobbyScreen extends HookWidget {
   const LobbyScreen({Key? key}) : super(key: key);
@@ -31,24 +34,78 @@ class LobbyScreen extends HookWidget {
     }, []);
 
     confirmLeaveRoom() {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(
-            controller.isHost
-                ? 'You are a host, do you want to close this room?'
-                : 'Do you want to leave the room?',
+      Get.showSnackbar(GetBar(
+        message: controller.isHost
+            ? 'Kamu adalah host room ini, ingin menutup room?'
+            : 'Yakin keluar dari room?',
+        backgroundColor: controller.isHost ? Colors.red : Colors.grey,
+        duration: 5.seconds,
+        animationDuration: 500.milliseconds,
+        snackStyle: SnackStyle.FLOATING,
+        snackPosition:
+            controller.isHost ? SnackPosition.TOP : SnackPosition.BOTTOM,
+        margin: const EdgeInsets.all(8),
+        borderRadius: 10.0,
+        onTap: (_) {
+          controller.leaveRoom();
+        },
+        mainButton: TextButton(
+          style: TextButton.styleFrom(
+            primary: kBackgroundColor,
           ),
-          action: SnackBarAction(
-            label: controller.isHost ? 'Close' : 'Leave',
-            onPressed: () {
-              controller.leaveRoom();
-            },
+          child: Text(
+            controller.isHost ? 'Tutup' : 'Keluar',
           ),
+          onPressed: () {
+            controller.leaveRoom();
+          },
         ),
-      );
+      ));
     }
 
     return Scaffold(
+      appBar: AppBar(
+        title: controller.isHost
+            ? TextField(
+                controller: roomNameController,
+                decoration: const InputDecoration.collapsed(
+                  hintText: 'Room name',
+                ),
+                style: Get.textTheme.headline6!.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+                textInputAction: TextInputAction.done,
+                onSubmitted: (_) {
+                  if (roomNameController.text.isNotEmpty) {
+                    controller.updateRoomName(roomNameController.text);
+                  }
+                },
+              )
+            : Text(
+                room?.name ?? '',
+                style: Get.textTheme.headline6!.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.close_rounded,
+              color: controller.isHost ? Colors.red : null,
+            ),
+            onPressed: () {
+              if (controller.isHost) {
+                confirmLeaveRoom();
+              } else {
+                // bypass snackbar
+                controller.leaveRoom();
+              }
+            },
+          ),
+        ],
+      ),
       body: WillPopScope(
         onWillPop: () async {
           confirmLeaveRoom();
@@ -63,80 +120,60 @@ class LobbyScreen extends HookWidget {
           child: SafeArea(
             child: SizedBox(
               width: double.infinity,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        if (user?.uid == room?.hostUID)
-                          Expanded(
-                            child: TextField(
-                              controller: roomNameController,
-                              decoration: const InputDecoration(
-                                hintText: 'Room name',
-                              ),
-                              textInputAction: TextInputAction.done,
-                              onSubmitted: (_) {
-                                if (roomNameController.text.isNotEmpty) {
-                                  controller
-                                      .updateRoomName(roomNameController.text);
-                                }
-                              },
-                            ),
-                          )
-                        else
-                          Text(room?.name ?? '-'),
-                        ElevatedButton(
-                          onPressed: () {
-                            if (controller.isHost) {
-                              confirmLeaveRoom();
-                            } else {
-                              // bypass snackbar
-                              controller.leaveRoom();
-                            }
-                          },
-                          style: ElevatedButton.styleFrom(
-                            primary: controller.isHost ? Colors.red : null,
-                          ),
-                          child: Text(controller.isHost ? 'Close' : 'Leave'),
-                        ),
-                      ],
-                    ),
-                    if (controller.message != null) Text(controller.message!),
-                    Expanded(
+              child: Column(
+                children: [
+                  if (controller.message != null) Text(controller.message!),
+                  Expanded(
+                    child: SingleChildScrollView(
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: Wrap(
+                        child: ResponsiveGridRow(
                           children: participants
                               .map(
-                                (participant) => ParticipantItem(
-                                  participant: participant,
-                                  bgColor: participant.uid == room?.hostUID
-                                      ? Colors.green
-                                      : participant.uid == user?.uid
-                                          ? Colors.blue
-                                          : Colors.grey,
+                                (participant) => ResponsiveGridCol(
+                                  xs: 6,
+                                  md: 3,
+                                  xl: 2,
+                                  child: Entry.offset(
+                                    duration: 0.5.seconds,
+                                    curve: Curves.easeInOutExpo,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: ParticipantItem(
+                                        participant: participant,
+                                        bgColor:
+                                            participant.uid == room?.hostUID
+                                                ? Colors.green
+                                                : participant.uid == user?.uid
+                                                    ? Colors.blue
+                                                : kPrimaryLightColor,
+                                        info: participant.uid == room?.hostUID
+                                            ? 'Host'
+                                            : participant.uid == user?.uid
+                                                ? 'Kamu'
+                                                : null,
+                                      ),
+                                    ),
+                                  ),
                                 ),
                               )
                               .toList(),
                         ),
                       ),
                     ),
-                    if (user?.uid == room?.hostUID)
-                      SizedBox(
-                        width: double.infinity,
-                        height: Get.height * 0.1,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            controller.startCountdown();
-                          },
-                          child: const Text('Start'),
-                        ),
+                  ),
+                  if (user?.uid == room?.hostUID)
+                    SizedBox(
+                      width: double.infinity,
+                      height: Get.height * 0.1,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          controller.startCountdown();
+                        },
+                        child: const Text('Start'),
                       ),
-                  ],
-                ),
+                    ),
+                ],
               ),
             ),
           ),
--- lib/presentation/widgets/participant_item.dart
+++ lib/presentation/widgets/participant_item.dart
@@ -1,26 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:quizpancasila/domain/entities/user.dart';
 
 class ParticipantItem extends StatelessWidget {
   final User participant;
   final Color? bgColor;
+  final String? info;
 
   const ParticipantItem({
     Key? key,
     required this.participant,
     this.bgColor,
+    this.info,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(8),
-      margin: const EdgeInsets.symmetric(vertical: 8),
       decoration: BoxDecoration(
         color: bgColor ?? Colors.white,
-        borderRadius: BorderRadius.circular(8),
+        // add soft box shadow
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey[300]!.withOpacity(0.5),
+            blurRadius: 5,
+            spreadRadius: 1,
+            offset: const Offset(0, 1),
+          ),
+        ],
       ),
-      child: Text(participant.name),
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 8),
+            child: Text(
+              participant.name,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          if (info != null)
+            Positioned(
+              right: 2,
+              bottom: 2,
+              child: Text(
+                info!,
+                style: Get.textTheme.caption,
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
