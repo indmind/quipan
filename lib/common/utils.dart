3e30c38dd7d34437ba8d2b51f89ab249f2603790
Tio Irawan <sattanationmail@gmail.com>
2021-10-29 10:40:46 +0700

    Change calculateScore function
    so that the minimum point can be changed

--- lib/common/utils.dart
+++ lib/common/utils.dart
@@ -1,11 +1,16 @@
 // linear mapping biasa
-int calculateScore(int maxPoint, int quizDurationMs, int answerDuration) {
+int calculateScore(int maxPoint, int quizDurationMs, int answerDuration,
+    [int minPoint = 100]) {
   // to prevent minus score
   if (answerDuration > quizDurationMs) {
     return 0;
   }
 
   final timeRemaining = quizDurationMs - answerDuration;
+  
+  return map(timeRemaining, 0, quizDurationMs, minPoint, maxPoint).round();
+}
 
-  return (maxPoint * timeRemaining / quizDurationMs).round();
+double map(int n, int start1, int stop1, int start2, int stop2) {
+  return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
 }
